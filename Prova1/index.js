4af9500b243c6b74854c2665daa178dba5ef2f49
Jhonatan Dias Farias <diasjhonatan24@gmail.com>
2024-04-20 10:31:00 -0300

    Criando função de Critico e Verificador de Vida

--- Prova1/index.js
+++ Prova1/index.js
@@ -20,6 +20,7 @@ createApp({
             } else {
                 this.heroi.vida -= 20;
             }
+            this.verificarVida();
         },
         defender(isHeroi) {
             // Adiciona uma chance de 2/3 do heroi defender o ataque do vilão
@@ -42,7 +43,7 @@ createApp({
             if (Math.random() < 1 / 3) {
                 console.log("Vilão atacou!");
                 this.acaoVilao();
-            } else if (Math.random() < 1 / 3) {
+            } else if (Math.random() < 2 / 3) {
                 console.log("Vilão usou poção!");
                 this.vilao.vida += 10;
                 // Limita a vida do vilão a 100
@@ -52,12 +53,40 @@ createApp({
             }
         },
         critico(isHeroi) {
-            this.acaoVilao();
+            // Ataque crítico que causa o dobro do dano do ataque normal
+            if (isHeroi) {
+                this.vilao.vida -= 20;
+                console.log("O herói deu um golpe crítico!");
+                // Adiciona uma chance de 1/3 do vilão revidar
+                if (Math.random() < 1 / 3) {
+                    console.log("Contra aquete do vilão!");
+                    this.acaoVilao();
+                }
+            } else {
+                this.heroi.vida -= 40;
+                console.log("O vilão deu um golpe crítico!");
+            }
         },
         acaoVilao() {
             const acoes = ['atacar', 'defender', 'usarPocao', 'critico'];
             const acaoAleatoria = acoes[Math.floor(Math.random() * acoes.length)];
             this[acaoAleatoria](false);
+            this.verificarVida();
+        },
+        verificarVida() {
+            // Se a vida de qual quer um chegar a 0 cria-se um pop-up alertando o vencedor!!
+            if (this.heroi.vida <= 0) {
+                alert("Vilão Ganhou");
+                this.recomecar();
+            } else if (this.vilao.vida <= 0) {
+                alert("Herói Ganhou");
+                this.recomecar();
+            }
+        },
+        recomecar() {
+            //Após o Alert o jogo recomeça com a vida 100% de ambos
+            this.heroi.vida = 100;
+            this.vilao.vida = 100;
         }
     }
 }).mount("#app");
